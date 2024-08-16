6c3e10c55c7303c5d0df533abf29ad3c70a838c2
AJTimePyro <ajtimepyro@gmail.com>
2024-08-16 22:35:48 +0530

    added tsx watch instead of nodemon

--- api/index.ts
+++ api/index.ts
@@ -4,4 +4,11 @@ import { Request, Response } from "express";
 const app = Express();
 app.use(Express.json());
 
-app.get("/", async (req: Request, res: Response) => {});
+app.get("/", async (req: Request, res: Response) => {
+  console.log(req);
+  console.log(res);
+});
+
+app.listen(3000, () => {
+  console.log(`Server is Fire at http://localhost: 3000`);
+});
