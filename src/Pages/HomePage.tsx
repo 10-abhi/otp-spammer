fc90720484a9b42889e4388973c2c63258647f65
Abhishek Mishra <abhishek81936@gmail.com>
2024-08-22 19:43:20 +0530

    Adding footer

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,11 +1,13 @@
+import { Footer } from "../components/footer"
 import { Navbar } from "../components/NavBar"
 import { Number } from "../components/Number"
 
 export const HomePage = ()=>{
  return <div className="h-screen w-screen bg-gradient-to-b from-slate-900 via-sky-900 to-gray-900">
-    <div>
         <Navbar></Navbar>
+    <div className="h-3/4">
         <Number></Number>
     </div>
+    <Footer></Footer>
  </div>
 }
