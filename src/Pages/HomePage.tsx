808f09be530feb3398ffc17a9ddf44784ebec5c1
10-abhi <abhishek81936@gmail.com>
2024-08-25 14:02:02 +0530

    Adding button

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,12 +1,17 @@
+import { Button } from "../components/Button"
 import { Footer } from "../components/footer"
 import { Navbar } from "../components/NavBar"
 import { Number } from "../components/Number"
 
 export const HomePage = ()=>{
- return <div className="h-screen w-screen bg-gradient-to-b from-slate-900 via-sky-900 to-gray-900">
+ return <div className="flex flex-col h-screen w-screen bg-gradient-to-b from-slate-900 via-sky-900 to-gray-900">
         <Navbar></Navbar>
-    <div className="h-3/4">
+    <div className="h-3/4 ">
         <Number></Number>
+        <div className="flex justify-center">
+            <Button></Button>
+        </div>
+        
     </div>
     <Footer></Footer>
  </div>
