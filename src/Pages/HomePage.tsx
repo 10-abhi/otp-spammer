12593370d60062a4e77a252b8867746353d388ac
10-abhi <abhishek81936@gmail.com>
2024-09-10 10:51:36 +0530

    Extracting targetNum from input

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -4,19 +4,21 @@ import { Navbar } from "../components/NavBar"
 import { Number } from "../components/Number"
 import { InputBox } from "../components/InputBox"
 import axios from "axios"
+import { SetStateAction, useState } from "react"
 
 
    
 export const HomePage = ()=>{
-  
+
+ const [targetNum , setTargetNum] = useState("");
  return <div className="flex flex-col h-screen w-screen bg-gradient-to-b from-slate-900 via-sky-900 to-gray-900">
         <Navbar></Navbar>
     <div className="h-3/4 px-10 py-10 flex items-center flex-col">
         <Number></Number>
-        <InputBox></InputBox>
+        <InputBox onChange={(e: { target: { value: SetStateAction<string> } })=>setTargetNum(e.target.value)}></InputBox>
         <div className="flex justify-center">
             <Button onClick={async()=>{
-                axios.get("http://localhost:3000/sms?target="+target)
+                axios.get("http://localhost:3000/sms?target="+targetNum)
             }} ></Button>
         </div>
         
--- src/components/InputBox.tsx
+++ src/components/InputBox.tsx
@@ -1,6 +1,6 @@
-export const InputBox = ()=>{
-       return  <div className="pt-0 pb-6">
+export const InputBox = ({onChange})=>{
+       return  <div className="pt-0 pb-6" >
               <input type="text" placeholder="8829201764" className="border-2
        
-       rounded-md hover:border-green-900 h-10 w-64 flex justify-center items-center"/></div>
+       rounded-md hover:border-green-900 h-10 w-64 flex justify-center items-center" onChange={onChange}/></div>
 }
